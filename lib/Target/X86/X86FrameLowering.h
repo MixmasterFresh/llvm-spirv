015516dd16eb56dc94aa48cc1a0c9483582e6ef4
Reid Kleckner <reid@kleckner.net>
2015-01-29 22:33:00 +0000

    Update comments to use unreachable instead of llvm.trap, as implemented now
    win64: Call __chkstk through a register with the large code model
    
    Fixes half of PR18582. True dynamic allocas will still have a
    CALL64pcrel32 which will fail.
    
    Reviewers: majnemer
    
    Differential Revision: http://reviews.llvm.org/D7267
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@227503 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Target/X86/X86FrameLowering.h
+++ lib/Target/X86/X86FrameLowering.h
@@ -27,11 +27,9 @@ public:
   explicit X86FrameLowering(StackDirection D, unsigned StackAl, int LAO)
     : TargetFrameLowering(StackGrowsDown, StackAl, LAO) {}
 
-  /// Emit a call to the target's stack probe function. This is required for all
-  /// large stack allocations on Windows. The caller is required to materialize
-  /// the number of bytes to probe in RAX/EAX.
-  static void emitStackProbeCall(MachineFunction &MF, MachineBasicBlock &MBB,
-                                 MachineBasicBlock::iterator MBBI, DebugLoc DL);
+  static void getStackProbeFunction(const MachineFunction &MF,
+                                    const X86Subtarget &STI, unsigned &CallOp,
+                                    const char *&Symbol);
 
   void emitCalleeSavedFrameMoves(MachineBasicBlock &MBB,
                                  MachineBasicBlock::iterator MBBI,

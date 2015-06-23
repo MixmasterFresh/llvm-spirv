af80c4ccaddf0b074445470704b7ac0899cb4d64
Alexander Kornienko <alexfh@google.com>
2015-06-23 09:49:53 +0000

    Revert r240137 (Fixed/added namespace ending comments using clang-tidy. NFC)
    Apparently, the style needs to be agreed upon first.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@240390 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/llvm/Transforms/Utils/SimplifyLibCalls.h
+++ include/llvm/Transforms/Utils/SimplifyLibCalls.h
@@ -167,6 +167,6 @@ private:
   /// function by checking for an existing function with name FuncName + f
   bool hasFloatVersion(StringRef FuncName);
 };
-} // namespace llvm
+} // End llvm namespace
 
 #endif
